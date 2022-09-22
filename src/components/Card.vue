6515afd4dbd5e1471c5ea90db4187a3a0cfee9b4
29050335 <a3521110036@alumno.uttehuacan.edu.mx>
2022-09-22 13:31:17 -0500

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <Grid>
+      <slot>Imagen</slot>
+      <h2>{{ nombre }}</h2> 
+      <h4>{{ descripcion }}</h4><br>
+      <h3>{{ costo }}</h3><br>
+  </Grid>
+</template>
+
+<script>
+  import Grid from "./Grid.vue";
+  export default{
+  props: {
+      nombre: {
+          String,
+      },
+      descripcion: {
+          String,
+      },
+      costo: {
+          String,
+      },
+  },
+  components: { Grid },
+};
+</script>
+
+<style scopet>
+
+  img{
+      width: 11rem;
+      height: 11rem;
+      padding: 0.1rem;
+      background: rgba(177, 177, 177, 0.297);
+  }
+ 
+  
+</style>
